a4c4ae56ebc5f48580ee4ea23283907125176183
mwilliams2099 <m.williams200799@gmail.com>
2021-12-04 10:06:34 +0000

    Private distortion variables set up

--- DistortionPlugin/Source/DistortionClass.h
+++ DistortionPlugin/Source/DistortionClass.h
@@ -9,3 +9,21 @@
 */
 
 #pragma once
+
+class DistortionClass
+{
+private:
+    float halfRectifier(float input);
+    float fullRectifier(float input);
+    float softClipper(float input);
+    float bitCrusher(float input);
+    float waveFolder(float input);
+    float rectInGain, rectThresh, rectOutGain;
+    float sClipInGain, posAlpha, sClipPosThresh, negAlpha, sClipNegThresh, sClipOutGain;
+    float crushSteps;
+    bool crushBypass;
+    float foldInGain, foldOutGain, folds, foldOffset;
+
+public:
+    float distortionProcess(float input);
+};
