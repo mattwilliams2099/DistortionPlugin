42c2bf3281629051c5b2f8f0e7b4223fc8021e9c
mwilliams2099 <m.williams200799@gmail.com>
2021-12-04 10:09:10 +0000

    Getters and setters added

--- DistortionPlugin/Source/DistortionClass.h
+++ DistortionPlugin/Source/DistortionClass.h
@@ -26,4 +26,45 @@ private:
 
 public:
     float distortionProcess(float input);
+
+    float getRectInGain()               { return rectInGain; }
+    float getRectThresh()               { return rectThresh; }
+    float getRectOutGain()              { return rectOutGain; }
+
+    void setRectInGain(float newVal)    { rectInGain = newVal; }
+    void setRectThresh(float newVal)    { rectThresh = newVal; }
+    void setRectOutGain(float newVal)   { rectOutGain = newVal; }
+
+    float getSClipInGain()              { return sClipInGain; }
+    float getPosAlpha()                 { return posAlpha; }
+    float getSClipPosThresh()           { return sClipPosThresh; }
+    float getNegAlpha()                 { return negAlpha; }
+    float getSClipNegThresh()           { return sClipNegThresh; }
+    float getSClipOutGain()             { return sClipOutGain; }
+
+    void setSClipInGain(float newVal)   { sClipInGain = newVal; }
+    void setPosAlpha(float newVal)      { posAlpha = newVal; }
+    void setSClipPosThresh(float newVal){ sClipPosThresh = newVal; }
+    void setNegAlpha(float newVal)      { negAlpha = newVal; }
+    void setSClipNegThresh(float newVal){ sClipNegThresh = newVal; }
+    void setSClipOutGain(float newVal)  { sClipOutGain = newVal; }
+
+    float getCrushSteps()               { return crushSteps; }
+    bool getCrushBypass()               { return crushBypass; }
+
+    void setCrushSteps(float newVal)    { crushSteps = newVal; }
+    void setCrushBypass(bool newBool)   { crushBypass = newBool; }
+
+    float getFoldInGain()               { return foldInGain; }
+    float getFoldOutGain()              { return foldOutGain; }
+    float getFolds()                    { return folds; }
+    float getFoldOffset()               { return foldOffset; }
+
+    void setFoldInGain(float newVal)    { foldInGain = newVal; }
+    void setFoldOutGain(float newVal)   { foldOutGain = newVal; }
+    void setFolds(float newVal)         { folds = newVal; }
+    void setFoldOffset(float newVal)    { foldOffset = newVal; }
+
+
+
 };
